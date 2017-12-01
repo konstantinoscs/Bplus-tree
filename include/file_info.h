50975ba83cfba4dcc50427100093bec6ff6390bb
Thanasis Filippidis <sdi1400215@di.uoa.gr>
2017-12-01 04:51:21 +0200

    Fixed the insert so the first time we insert something the root gains its first key so after that everything works typically, from now on we have a rootInitialized var in the file info struct and in the metadata block

--- include/file_info.h
+++ include/file_info.h
@@ -11,13 +11,14 @@ typedef struct file_info{
   int type2;
   int length2;
   int root_id;
+  int rootInitialized;
 }file_info;
 
 extern file_info * openFiles[20];
 
 //insert_file takes the name of a file and inserts it into openFiles
 //if openFiles is fulll then -1 is returned
-int insert_file(int type1, int length1, int type2, int length2);
+int insert_file(int type1, int length1, int type2, int length2, int rootInitialized);
 
 //insert_bdf inserts a file_desc file_info
 void insert_bfd(int fileDesc, int bf_desc);
