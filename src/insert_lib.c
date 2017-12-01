0487c087f6cbd00686380f704f7b83d6c5160fda
konstantinoscs <kons.ste@gmail.com>
2017-12-01 04:49:33 +0200

    compiles ok

--- src/insert_lib.c
+++ src/insert_lib.c
@@ -1,15 +1,20 @@
+#include <string.h>
+#include <stdlib.h>
 #include "bf.h"
 #include "file_info.h"
 #include "stack.h"
-//circular depndence
+#include "BoolType.h"
 #include "AM.h"
 
 extern file_info * openFiles[20];
 
 //the declarations I will need, definitions are a the end of this file
+int block_has_space(char * data, int keysize, int keys);
 int find_offset(char* data, int keysize, void *key, int keytype);
 //checks if we are in the root
-int is_root(int block_no. int fileDesc);
+int is_root(int block_no, int fileDesc);
+//the standard initialization for an inside node
+int initialize_block(char * data, int block_id);
 
 //inserts an indexing value
 int insert_index_val(void *value, int fileDesc, Stack** stack, int newbid){
@@ -18,12 +23,13 @@ int insert_index_val(void *value, int fileDesc, Stack** stack, int newbid){
   //get the number of the bf file we're in
   int bf_no = openFiles[fileDesc]->bf_desc;
   //get current block number from stack
-  int block_no = (*stack)->get_top();
+  int block_no = get_top((*stack));
   //open block and get its data
   BF_GetBlock(bf_no, block_no, curBlock);
   char * data = BF_Block_GetData(curBlock);
   //get the number of keys because we may have to update the number later
   int offset = sizeof(bool) + 2*sizeof(int);
+  int keys = 0;
   memmove(&keys, data+offset, sizeof(int));
   //add an int to offset to compute the total size
   offset += sizeof(int);
@@ -41,10 +47,11 @@ int insert_index_val(void *value, int fileDesc, Stack** stack, int newbid){
     memmove(data+offset, value, keysize);
     offset += keysize;
     //here I should have which block is beneath
-    memmove(data+offset, newbid ,sizeof(int));
+    memmove(data+offset, &newbid ,sizeof(int));
     //now set offset in the correct position to update the number of keys=
     offset = sizeof(bool) + 2*sizeof(int);
-    memmove(data+offset, &++keys, sizeof(int));
+    keys++;
+    memmove(data+offset, &keys, sizeof(int));
     //done :)
     BF_Block_SetDirty(curBlock);
   }
@@ -52,9 +59,33 @@ int insert_index_val(void *value, int fileDesc, Stack** stack, int newbid){
     //split and update root
     BF_Block *newBlock;
     BF_Block_Init(&newBlock);
+    BF_AllocateBlock(bf_no, newBlock);
+    int new_id = 0;
+    BF_GetBlockCounter(bf_no, &new_id);
+    new_id--;
+    char * new_data = BF_Block_GetData(newBlock);
+    initialize_block(new_data, new_id);
+    //keys in 1st is ceil(key2+1/2)
+    int keys_in1st = (keys+1)%2 ? (keys+1)/2 +1 : (keys+1)/2;
+    int keys_in2nd = keys_in1st -1;
+    //obviously in root there will be 1 key
+    char * mid_key = malloc(keysize);
+    //get key to up here
+    //find middle key,
+    //partition :)
+    //workds also for the recursion
+    //magic exists :)
+    //copy half of the values
+    //
+    //update the root_id in openFiles
+    //don't forget the metadata
+
+    free(mid_key);
   }
   else{
     //split and pass one level up
+    BF_Block *newBlock;
+    BF_Block_Init(&newBlock);
   }
 
   //close/unpnin block ;)
@@ -101,6 +132,29 @@ int find_offset(char* data, int keysize, void *key, int keytype){
 }
 
 //checks if we are in the root
-int is_root(int block_no. int fileDesc){
+int is_root(int block_no, int fileDesc){
   return openFiles[fileDesc]->root_id == block_no;
+}
+
+//the standard initialization for an inside node
+int initialize_block(char * data, int block_id){
+  bool isLeaf = false;
+  int nextPtr = -2;
+  int recordsNum = 0;
+  int offset = 0;
+  memmove(data, &isLeaf, sizeof(bool));
+  offset += sizeof(bool);
+  memmove(data+offset, &block_id, sizeof(int));
+  offset += sizeof(int);
+  memmove(data+offset, &nextPtr, sizeof(int));
+  offset += sizeof(int);
+  memmove(data+offset, &recordsNum, sizeof(int));
+  return 1;
+}
+
+int find_middle_key(char * data, char * mid_key, int keysize, int keys){
+  int offset = sizeof(bool) + 3*sizeof(int);
+  for(int i=0; i<keys; i++){
+
+  }
 }
