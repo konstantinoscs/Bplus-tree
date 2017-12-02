72c19bf66be6c5e660beedb49de4ff0802877531
NedicNemanja <sdi1400124@di.uoa.gr>
2017-12-02 04:16:42 +0200

    Fixed bug in insert when splitting a leaf block. Test now has strings as a key.

--- examples/test.c
+++ examples/test.c
@@ -7,11 +7,13 @@
 
 void main(void){
   AM_Init();
-  AM_CreateIndex("TestBase", INTEGER, sizeof(int), INTEGER, sizeof(int));
+  AM_CreateIndex("TestBase", STRING, 5, INTEGER, sizeof(int));
   int fd = AM_OpenIndex("TestBase");
 
+
   for(int i=0; i<1000; i++){
-    printf("(((%d)))", i);
+    char str[5]= { '0'+i/1000, '0'+i/100 ,'0'+i/10, '0'+i%10,'\0'};
+    printf("(((%s)))", str);
     AM_InsertEntry(fd,(void*) &i, (void*) &i);
   }
   AM_CloseIndex(fd);
